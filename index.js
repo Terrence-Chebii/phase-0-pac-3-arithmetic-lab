1e3839eeeff5b305193c1c7f0a4a91b550cb5b6e
bigman <terrence.chebii@student.moringaschool.com>
2023-03-12 09:05:25 +0300

    done

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a , b){
+    return a + b
+}
+function subtract(a , b){
+    return a - b
+}
+function multiply(a , b){
+    return a * b
+}
+function divide(a , b){
+    return a / b
+}
+
+function increment(n){
+    return ++n
+}
+
+function decrement(n){
+    return --n
+}
+
+function makeInt(n){
+    parseInt("0" ,10);
+    return n
+}
+
+function preserveDecimal(n){
+   parseFloat("2.222", 10) 
+   return n
+}
